4756456fdfec88d103c3a7514feeae6178eaf337
Eddie <eddie-hart@hotmail.com>
2024-03-20 18:32:48 -0600

    reformatted code

--- src/Time.java
+++ src/Time.java
@@ -6,18 +6,21 @@ public class Time {
     private int hour;
     private int minute;
     private int second;
+
     // time object that defaults to midnight
     public Time() {
         this.hour = 0;
         this.minute = 0;
         this.second = 0;
     }
+
     // time object that takes input for a custom time
     public Time(int hour, int minute, int second) {
         this.hour = hour;
         this.minute = minute;
         this.second = second;
     }
+
     // here the print method uses an if else statement to determine whether it will be military time or not.
     public void print(boolean military) {
         if (military) {
@@ -25,13 +28,14 @@ public class Time {
         } else {
             // heres a little nested if else statement to make sure it displays PM and AM properly
             if (this.hour >= 12) {
-                this.hour -=  12;
+                this.hour -= 12;
                 System.out.printf("%02d:%02d:%02d PM \n", this.hour, this.minute, this.second);
             } else {
                 System.out.printf("%02d:%02d:%02d AM \n", this.hour, this.minute, this.second);
             }
         }
     }
+
     /* the increment method was the hardest part of the program for me to figure out,
     and im still not quite happy with it, although it works with the given times for the assignment.
     it was hard to figure out how to make it so that it would display proper changing of time and not weird
@@ -55,17 +59,18 @@ public class Time {
                 this.hour += 1;
             }
         }
-        }
-        // here the fromString method will split a time string at the colon
-        // the parseInt method will check the int at the desired index from the array and store them in their respective variable
-        // the method returns a Time object with the previously given string
-        public static Time fromString(String input) {
-            String[] splitstring = input.split(":");
-            int Hour = Integer.parseInt(splitstring[0]);
-            int Minute = Integer.parseInt(splitstring[1]);
-            int Second = Integer.parseInt(splitstring[2]);
-            return new Time(Hour, Minute, Second);
-        }
+    }
+
+    // here the fromString method will split a time string at the colon
+    // the parseInt method will check the int at the desired index from the array and store them in their respective variable
+    // the method returns a Time object with the previously given string
+    public static Time fromString(String input) {
+        String[] splitstring = input.split(":");
+        int Hour = Integer.parseInt(splitstring[0]);
+        int Minute = Integer.parseInt(splitstring[1]);
+        int Second = Integer.parseInt(splitstring[2]);
+        return new Time(Hour, Minute, Second);
+    }
 }
 
 
